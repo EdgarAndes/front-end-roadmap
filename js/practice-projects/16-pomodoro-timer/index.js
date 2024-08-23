32b3280d62fe6ae9a8740d83189da183d0a0a267
fesh <eandes@adtorqueedge.com>
2024-08-23 23:16:58 +0800

    update

--- js/practice-projects/16-pomodoro-timer/index.js
+++ js/practice-projects/16-pomodoro-timer/index.js
@@ -4,7 +4,7 @@ const resetEl = document.getElementById("reset");
 const timerEl = document.getElementById("timer");
 
 let interval;
-let timeLeft = 5;
+let timeLeft = 1500;
 
 function updateTimer(){
     let minutes = Math.floor(timeLeft / 60);
@@ -28,7 +28,7 @@ function startTimer(){
 }
 
 function stopTimer(){
-    clearInterval(interval);
+    clearInterval(interval); /* stop timer */
 }
 
 function resetTimer(){
